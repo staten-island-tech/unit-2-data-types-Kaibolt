dbd70dbf4dff44915c47724b62037a99b1956a77
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2023-09-20 13:59:52 -0400

    Create madlibthing1.py

---
+++ madlibthing1.py
@@ -0,0 +1,33 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape("turtle")
+
+def message(imput):
+    print(imput)
+
+def adj(x):
+    message("")
+
+def food(x):
+    message("")
+
+def name(x):
+    message("")
+
+def noun1(x):
+    message("")
+
+def noun2(x):
+    message("")
+
+def verb1(x):
+    message("")
+
+def verb2(x):
+    message("")
+
+def verb3(x):
+    message("")
+
