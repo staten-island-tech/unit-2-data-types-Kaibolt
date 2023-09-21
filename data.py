99a0981f032bb9e7f875446a24a6302e0659878a
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2023-09-21 13:58:58 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,28 @@
+x = 3
+y = float(3)
+print(x,y)
+
+values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6])
+## Doing print(values[7]) does not work as there is no 7th value.
+
+x = "this is a thing"
+y = x.split()
+z = y[0]
+print(y)
+print(z)
+print("try it bro")
+## Get user to type sentence
+## 4 tries later. This is torture.
+x = input()
+y = x.split()
+z = y[0]
+values = y
+for o in values:
+    print(o)
+ 
