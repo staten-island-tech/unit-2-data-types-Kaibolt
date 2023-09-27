41d4d5bcd0f0195a0852adfe88598668116bb151
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2023-09-27 13:09:18 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+# This is for me to learn how to make the calculator because I dont understand it.
 print("Hi")
 name = input('What is your name?' )
 print(name)
@@ -10,3 +11,18 @@ print("ez")
 
 name2 = input('Please tell me your name: ')
 print(name2)
+print('Blank line \nin the middle of string')
+# /n makes a line mid sentence, crazy
+first_name = input('What is your first name? ')
+last_name = input('Now give me your last name. ')
+print(first_name.capitalize() + ' ' + last_name.lower())
+# .capatalize() gives the first letter capatal, and .lower makes the rest of it lowercase while the ' ' makes a word thats empty making it a space. i think
+# variable.count(letter or number) counts the amount of time the thing in parathesis shows up
+# challange thing time
+n1 = input('give me a number ')
+n2 = input('gimmie another ')
+a1 = (float(n1) + float(n2))
+print(a1)
+#float makes it a number i think
+print('hi\n\n\n\n\n\n\n\n\nbye')
+# int is whole number and float is decimal
